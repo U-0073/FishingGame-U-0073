7ab93e6eee0526017d16480f36295af32480c382
s5016tpro8901016 <s5016tpro8901016@gmail.com>
2020-04-18 16:36:56 +0900

    ホワイトアウトはできるでもおかしい

--- src/Game/CTitleScene.cpp
+++ src/Game/CTitleScene.cpp
@@ -3,6 +3,9 @@
 
 CTitleScene::CTitleScene()
 {
+	KD3D.LoadTexture(&vTex, "Resouce/Texture/white.png", 10000,10000, D3DCOLOR_ARGB(255, 255, 255, 255));
+	D3DXMatrixTranslation(&mMat, 0.0f, 0.0f, 0.0f);
+	
 	Sky = std::make_shared<Skysphere>();
 	Sky->Init();
 	Fishes = std::make_shared<Fish>();
@@ -12,7 +15,10 @@ CTitleScene::CTitleScene()
 	
 	CAMERA.SetCameraPos(D3DXVECTOR3(0,3,-5), Fishes->GetFishPos());
 	MoveFlg = false;
-	Cnt = 0;
+	Alpha = 0;
+
+	mPos = D3DXVECTOR3(0, 20, 0);
+
 }
 
 CTitleScene::~CTitleScene()
@@ -37,11 +43,16 @@ int CTitleScene::Update()
 		//return GAME;
 	}
 	if (MoveFlg) {
+		if (Fishes->GetFishPos().z < -30) {
+			Alpha += 0.5;
+		}
+		if (Alpha > 255) {
+			Alpha = 255;
+		}
 		Fishes->TitleUpdate();
 		CAMERA.SetCameraPos(D3DXVECTOR3(0, 3, -5), Fishes->GetFishPos());
-		Cnt++;
 	}
-	if (Cnt > 500) {
+	if (Fishes->GetFishPos().z < -100) {
 		return GAME;
 	}
 	return TITLE;
@@ -51,6 +62,12 @@ void CTitleScene::Draw2D()
 {
 	Fishes->Draw2D();
 	title->Draw2D();
+	
+	SPRITE->Begin(D3DXSPRITE_ALPHABLEND);
+	RECT rcTitle = { 0,0,10000,10000 };
+	SPRITE->SetTransform(&mMat);
+	SPRITE->Draw(vTex, &rcTitle, &D3DXVECTOR3(5000.0f, 5000.0f, 0.0f), NULL, D3DCOLOR_ARGB((int)Alpha, 255, 255, 255));
+	SPRITE->End();
 }
 
 void CTitleScene::Draw3D()
