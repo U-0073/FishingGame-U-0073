bea9801458ef293ef59746e9cff66cdf69d0c042
雄也 山口 <kd1264526@st.kobedenshi.ac.jp>
2020-04-20 15:18:27 +0900

    CShopScene追加

---
+++ src/Game/CShopScene.h
@@ -0,0 +1,27 @@
+#pragma once
+#include"../System/FrameBase/CSceneBase.h"
+#define path "Resouce/Texture/"
+#include"Shop.h"
+
+//class Shop;
+
+class CShopScene : public CSceneBase
+{
+public:
+	CShopScene();
+	~CShopScene();
+
+	void Init()override;
+	int Update()override;
+	void Draw2D()override;
+	void Draw3D()override;
+
+
+private:
+	//std::shared_ptr<Shop> shop = nullptr;
+
+	D3DXMATRIX mMat;
+	D3DXMATRIX mScale;
+	LPDIRECT3DTEXTURE9 vTex;
+
+};
