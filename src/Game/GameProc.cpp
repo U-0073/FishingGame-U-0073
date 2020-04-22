aca6e0ca65e3edf1de90f43e00b6f4f94bda3fe1
Bikkyy <kd1267125@st.kobedenshi.ac.jp>
2020-04-22 18:01:31 +0900

    画像のW,Hがいらない読み込み関数をオーバーロードで作ってみた

---
+++ src/Game/GameProc.cpp
@@ -0,0 +1,3 @@
+#include"../System/KdLibrary/KdLibrary.h"
+#include"GameProc.h"
+
---
+++ src/Game/GameProc.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class CGameProc
+{
+	CGameProc();
+	~CGameProc();
+	void Init();
+	void Update();
+	void Draw();
+	void End();
+
+private:
+	LPDIRECT3DTEXTURE9 ringTex;
+	LPDIRECT3DTEXTURE9 notesTex;
+
+	LPDIRECT3DTEXTURE9 JudgeTex;
+
+};
