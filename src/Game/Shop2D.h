79ed0d3ac5a1b4ef3c27ab15699585bf734a5fe3
武士 脇 <kd1204197@st.kobedenshi.ac.jp>
2020-04-26 00:50:54 +0900

    ショップの背景、リスト、キーによる選択の追加

--- src/Game/Shop2D.h
+++ src/Game/Shop2D.h
@@ -15,8 +15,14 @@ public:
 	void Draw3D()override;
 
 private:
-	D3DXMATRIX mMat;//ƒ^ƒCƒgƒ‹
-	LPDIRECT3DTEXTURE9 vTex;
+	D3DXMATRIX FrameMat,TabLeftMat, TabCenterMat, TabRightMat,ListSelectMat;
+	D3DXMATRIX ListMat[6];
+	LPDIRECT3DTEXTURE9 FrameTex, TabLeftTex, TabCenterTex, TabRightTex, ListSelectTex;
+	LPDIRECT3DTEXTURE9 ListTex[6];
 	D3DXVECTOR3 mPos;
+
+	float select[6];
+
+	bool KeyDownFlg = false, KeyUpFlg = false;
 };
 
