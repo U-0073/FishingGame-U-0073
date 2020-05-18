6c88d6d5ec81a0f49c27863d0034a51f774642a7
雄也 山口 <kd1264526@st.kobedenshi.ac.jp>
2020-05-18 11:03:02 +0900

    一時保存

--- src/Game/Seagull.cpp
+++ src/Game/Seagull.cpp
@@ -16,16 +16,26 @@ void Seagull::Init()
 	ScaleMat.CreateScale(0.1f, 0.1f, 0.1f);
 	TransMat.CreateTrans(0.0f, 0.0f, 0.0f);
 	m_world = ScaleMat * TransMat;
-	m_pModel = RESOURCE_MNG.GetModel(SEagull);
-
+	m_pModel = RESOURCE_MNG.GetModel("Resource/3DModel/Seagull.x");
 }
 
 void Seagull::Update()
 {
+	SeagullLen = KdVec3(SeagullPos.x, SeagullPos.y+50, SeagullPos.z);
+	KdVec3 m, n;
+
+	float len = m.LengthToTarget(n);
+
 }
 
 void Seagull::Draw2D()
 {
+	SPRITE->End();
+	RECT rcText = { 10,30 * 0,0,0 };
+	char Text[10];
+	sprintf_s(Text, sizeof(Text), "SeagullPos x %f y %f z %f", SeagullPos.x, SeagullPos.y, SeagullPos.z);
+	FONT->DrawText(NULL, Text, -1, &rcText, DT_LEFT | DT_NOCLIP, D3DCOLOR_XRGB(255, 255, 255));
+	SPRITE->Begin(D3DXSPRITE_ALPHABLEND);
 }
 
 void Seagull::Draw3D()
