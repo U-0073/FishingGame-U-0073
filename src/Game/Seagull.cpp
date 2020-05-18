853f9aa7a3adad613c6914e4e48f4a5e66563609
雄也 山口 <kd1264526@st.kobedenshi.ac.jp>
2020-05-18 10:25:02 +0900

    一時プッシュします。

--- src/Game/Seagull.cpp
+++ src/Game/Seagull.cpp
@@ -11,6 +11,13 @@ Seagull::~Seagull()
 
 void Seagull::Init()
 {
+	GameObject::Init();
+
+	ScaleMat.CreateScale(0.1f, 0.1f, 0.1f);
+	TransMat.CreateTrans(0.0f, 0.0f, 0.0f);
+	m_world = ScaleMat * TransMat;
+	m_pModel = RESOURCE_MNG.GetModel(SEagull);
+
 }
 
 void Seagull::Update()
@@ -23,8 +30,14 @@ void Seagull::Draw2D()
 
 void Seagull::Draw3D()
 {
+	KD3D.SetWorldMatrix(&m_world);
+
+	KD3D.GetDev()->SetRenderState(D3DRS_LIGHTING, TRUE);
+	m_pModel->Draw();
+	KD3D.GetDev()->SetRenderState(D3DRS_LIGHTING, FALSE);
 }
 
 void Seagull::End()
 {
+	m_pModel = nullptr;
 }
--- src/Game/Seagull.h
+++ src/Game/Seagull.h
@@ -3,18 +3,19 @@
 
 #define path "Resource/3DModel/"
 
-class Seagull
+class Seagull : GameObject
 {
 public:
 	Seagull();
 	~Seagull();
-	void Init();
-	void Update();
-	void Draw2D();
-	void Draw3D();
-	void End();
+	void Init()override;
+	void Update()override;
+	void Draw2D()override;
+	void Draw3D()override;
+	void End()override;
 
 private:
+	std::string SEagull;
 
 	KdVec3 SeagullPos;
 	KdVec3 SeagullLen;
