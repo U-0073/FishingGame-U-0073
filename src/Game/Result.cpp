848d395dce5c1d76d0842250de0e1cdd2134b72a
s5016tpro8901016 <s5016tpro8901016@gmail.com>
2020-05-27 14:46:25 +0900

    Result

--- src/Game/Result.cpp
+++ src/Game/Result.cpp
@@ -15,19 +15,18 @@ void Result::Init()
 {
 	ScoreData = DTWHOUCE.GetVec("score");
 	if (ScoreData.Success) {
-		vTex = RESOURCE_MNG.GetTexture("board.png", 1000, 430, NULL);
+		vTex = RESOURCE_MNG.GetTexture("board.png");
 		mPos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-		D3DXMatrixTranslation(&mMat, 0.0f, 0.0f, 0.0f);
+		mMat.SetTrans(0.0f, 0.0f, 0.0f);
 	}
 	else {
-		vTex = RESOURCE_MNG.GetTexture("escape.png", 336, 200, NULL);
+		vTex = RESOURCE_MNG.GetTexture("escape.png");
 		mPos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-		D3DXMatrixTranslation(&mMat, 0.0f, 0.0f, 0.0f);
+		mMat.SetTrans(0.0f, 0.0f, 0.0f);
 		mMat.SetScale(2.0f, 2.0f, 2.0f);
-		vBackTex = RESOURCE_MNG.GetTexture("SeaBack001.png", 1280, 720, NULL);
+		vBackTex = RESOURCE_MNG.GetTexture("SeaBack001.png");
 		mBackPos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-		D3DXMatrixTranslation(&mBackMat, 0.0f, 0.0f, 0.0f);
-
+		mMat.SetTrans(0.0f, 0.0f, 0.0f);
 	}
 }
 
@@ -36,13 +35,13 @@ void Result::Update()
 	if (ScoreData.Success) {
 		mPos = D3DXVECTOR3(1280.0f / 2, 720.0f / 3 * 2.3, 0.0f);
 		D3DXMatrixTranslation(&mMat, mPos.x, mPos.y, mPos.z);
+		mMat.SetTrans(mPos.x, mPos.y, mPos.z);
 	}
 	else {
 		mPos = D3DXVECTOR3(1280.0f / 2, 720.0f /2, 0.0f);
-		D3DXMatrixTranslation(&mMat, mPos.x, mPos.y, mPos.z);
-		
+		mMat.SetTrans(mPos.x, mPos.y, mPos.z);
 		mBackPos = D3DXVECTOR3(1280.0f / 2, 720.0f /2, 0.0f);
-		D3DXMatrixTranslation(&mBackMat, mBackPos.x, mBackPos.y, mBackPos.z);
+		mMat.SetTrans(mBackPos.x, mBackPos.y, mBackPos.z);
 	}
 }
 
